685defe0900b2c797fac1586194fe152697df68f
Maia Johnson <maiaj1306@gmail.com>
2022-09-14 12:46:50 -0500

    created function addToCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,3 +1,11 @@
 console.log('***** Music Collection *****')
 
 let collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+    let album = [title, artist, yearPublished]
+    collection.push(album);
+    return;
+}
+
+addToCollection('Currents', 'Tame Impala', '2015');
