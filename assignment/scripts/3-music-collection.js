08b33fe993cfdb399d7a14251962b0996bd087f6
Maia Johnson <maiaj1306@gmail.com>
2022-09-14 12:53:10 -0500

    added to and logged collection array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -9,3 +9,10 @@ function addToCollection(title, artist, yearPublished) {
 }
 
 addToCollection('Currents', 'Tame Impala', '2015');
+addToCollection('21st Century Breakdown', 'Green Day', '2009');
+addToCollection('RAZZMATAZZ', 'IDKHOW', '2020');
+addToCollection('you ruined new york city for me', 'FLETCHER', '2019');
+addToCollection('Ruin', 'The Amazing Devil', '2021');
+addToCollection('Runaway, Run', 'molly ofgeography', '2020');
+
+console.log(collection);
